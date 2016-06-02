ad5538586a7c81209302b1d39e4a9634298d17fb
Anik1199 <anik9280@gmail.com>
2016-06-02 19:43:07 +0600

    ido: Add basic keylayout

--- device.mk
+++ device.mk
@@ -104,6 +104,13 @@ PRODUCT_PACKAGES += \
     memtrack.msm8916 \
     liboverlay
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/ft5x06_ts.kl \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/synaptics_dsx.kl:system/usr/keylayout/synaptics_dsx.kl \
+    $(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl
+    
 # Keymaster
 PRODUCT_PACKAGES += \
     keystore.msm8916
