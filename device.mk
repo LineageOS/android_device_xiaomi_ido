6b549e693e3b05d705d1b1fb4cec77c4c6af2bbb
Anik1199 <anik9280@gmail.com>
2016-07-18 17:23:58 +0600

    ido: Build stlport

--- device.mk
+++ device.mk
@@ -102,7 +102,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     camera.msm8916 \
     libshim_camera \
-    libboringssl-compat
+    libboringssl-compat \
+    libstlport
 
 # Charger
 PRODUCT_PACKAGES += \
