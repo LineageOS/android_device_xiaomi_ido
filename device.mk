5b29c3ffd3d4673d1ec1476fe429e6602f0799f9
Danny Baumann <dannybaumann@web.de>
2016-07-18 17:22:33 +0600

    ido: Add the boringssl-compat lib for the JPEG encoder

--- device.mk
+++ device.mk
@@ -101,7 +101,8 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8916 \
-    libshim_camera
+    libshim_camera \
+    libboringssl-compat
 
 # Charger
 PRODUCT_PACKAGES += \
