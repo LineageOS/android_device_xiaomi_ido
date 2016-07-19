2b8d6eded3ddb5ca9a52d86ecb99f807978d31bd
Anik1199 <anik9280@gmail.com>
2016-07-19 11:12:57 +0600

    ido: Disable block based OTA

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,6 +42,9 @@ TARGET_USES_64_BIT_BINDER := true
 TARGET_BOOTLOADER_BOARD_NAME := MSM8916
 TARGET_NO_BOOTLOADER := true
 
+# Block Based OTA
+BLOCK_BASED_OTA := false
+
 # Kernel
 BOARD_DTBTOOL_ARGS := -2
 BOARD_KERNEL_CMDLINE := console=115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
