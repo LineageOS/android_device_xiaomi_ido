444c47cf1b5845c2285e61c6b872cd9fe1016090
Meninblack007 <sanyam.53jain@gmail.com>
2016-07-26 17:34:43 +0600

    ido: Import init.qcom.power.rc script

--- device.mk
+++ device.mk
@@ -199,7 +199,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.qcom.bt.sh \
-    init.qcom.power.sh \
+    init.qcom.power.rc \
     init.qcom.rc \
     init.qcom.sh \
     init.qcom.usb.rc \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -17,11 +17,11 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.power.sh
+LOCAL_MODULE       := init.qcom.power.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.power.sh
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_ETC)
+LOCAL_SRC_FILES    := etc/init.qcom.power.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
