ce1d4ee76fcb3b86a6b5cfcfd8356d17f5a7770c
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-09-02 10:54:33 +0200

    ido: gps: HAX
    * We can't use aosp GnssSvStatus struct
      -> Rename it to QcomSvStatus
    
    Change-Id: Ie79c5471423ce0d39e34f1dd0a3e71d9edb016b0

--- gps/core/UlpProxyBase.h
+++ gps/core/UlpProxyBase.h
@@ -64,7 +64,7 @@ public:
                                        LocPosTechMask loc_technology_mask) {
         return false;
     }
-    inline virtual bool reportSv(GnssSvStatus &svStatus,
+    inline virtual bool reportSv(QcomSvStatus &svStatus,
                                  GpsLocationExtended &locationExtended,
                                  void* svExt) {
         return false;
