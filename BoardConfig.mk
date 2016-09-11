d4ac192286dcf779558edd84db782546cbe3bf7b
Anik1199 <anik9280@gmail.com>
2016-09-11 20:24:49 +0600

    ido: Rule them all!

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,7 +41,7 @@ TARGET_NO_BOOTLOADER := true
 
 # Kernel
 BOARD_DTBTOOL_ARGS := -2
-BOARD_KERNEL_CMDLINE := console=115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_RAMDISK_OFFSET := 0x02000000
