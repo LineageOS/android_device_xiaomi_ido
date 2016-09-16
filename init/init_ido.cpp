e91a5e8dd5e27a9c8ce04a2f91c549a3b4dd10ea
Anik1199 <anik9280@gmail.com>
2016-09-16 23:23:51 +0600

    ido: Enable power-on alarm feature

--- init/init_ido.cpp
+++ init/init_ido.cpp
@@ -25,10 +25,18 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <sys/sysinfo.h>
 
+#include "vendor_init.h"
 #include "property_service.h"
+#include "log.h"
+#include "util.h"
 
+char const *device;
+char const *family;
 char const *heapstartsize;
 char const *heapgrowthlimit;
 char const *heapsize;
@@ -55,9 +63,41 @@ void check_device()
     }
 }
 
+static void init_alarm_boot_properties()
+{
+    int boot_reason;
+    FILE *fp;
+
+    fp = fopen("/proc/sys/kernel/boot_reason", "r");
+    fscanf(fp, "%d", &boot_reason);
+    pclose(fp);
+
+    /*
+     * Setup ro.alarm_boot value to true when it is RTC triggered boot up
+     * For existing PMIC chips, the following mapping applies
+     * for the value of boot_reason:
+     *
+     * 0 -> unknown
+     * 1 -> hard reset
+     * 2 -> sudden momentary power loss (SMPL)
+     * 3 -> real time clock (RTC)
+     * 4 -> DC charger inserted
+     * 5 -> USB charger inserted
+     * 6 -> PON1 pin toggled (for secondary PMICs)
+     * 7 -> CBLPWR_N pin toggled (for external power supply)
+     * 8 -> KPDPWR_N pin toggled (power key pressed)
+     */
+     if (boot_reason == 3) {
+        property_set("ro.alarm_boot", "true");
+     } else {
+        property_set("ro.alarm_boot", "false");
+     }
+}
+
 void vendor_load_properties()
 {
     check_device();
+    init_alarm_boot_properties();
 
     property_set("dalvik.vm.heapstartsize", heapstartsize);
     property_set("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
