7ee165d6792d826bf22b0701bfb0f02dea87622b
Sergey Karbainov <SPORTAK123@gmail.com>
2016-09-20 15:24:10 +0800

    ido: Update for Audio
    * Nuke aanc_tuning_mixer.txt
    * Add ACDB Data id
    * Add FM Tuner
    * Fix Headphone gain
    * Import a script for lineout audio
    * Update audio properties
    * Nuke unused soundtrigger mixer
    
    Match configs with Redmi 2

--- device.mk
+++ device.mk
@@ -83,12 +83,10 @@ PRODUCT_PACKAGES += \
     
 # Audio configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths_skuk.xml \
-    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
-    $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths_skuk.xml
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
@@ -188,6 +186,7 @@ PRODUCT_PACKAGES += \
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
+    init.lineout.sh \
     init.qcom.bt.sh \
     init.qcom.power.rc \
     init.qcom.rc \
