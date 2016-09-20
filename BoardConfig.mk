d49e24c70ab5d0735dec76bb8eee4d98948ffc14
Anik1199 <anik9280@gmail.com>
2016-09-20 14:27:04 +0600

    ido: Disable wifi as module

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -164,19 +164,18 @@ TARGET_HAVE_SIGNED_VENUS_FW := true
 
 # Wifi
 BOARD_HAS_QCOM_WLAN              := true
+BOARD_HAS_QCOM_WLAN_SDK          := true
 BOARD_HOSTAPD_DRIVER             := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_qcwcn
-TARGET_USES_QCOM_WCNSS_QMI       := true
-BOARD_HAS_QCOM_WLAN_SDK          := true
+BOARD_WLAN_DEVICE                := qcwcn
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
-BOARD_WLAN_DEVICE                := qcwcn
 TARGET_USES_WCNSS_CTRL           := true
+TARGET_USES_QCOM_WCNSS_QMI       := true
+TARGET_PROVIDES_WCNSS_QMI        := true
 WIFI_DRIVER_FW_PATH_AP           := "ap"
 WIFI_DRIVER_FW_PATH_STA          := "sta"
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
-WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wlan.ko"
-WIFI_DRIVER_MODULE_NAME          := "wlan"
 
 # inherit from the proprietary version
 -include vendor/xiaomi/ido/BoardConfigVendor.mk
