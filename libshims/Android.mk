a4b6fa399c7d0e430abb549b6d4e4e4f100ad6a8
Ethan Chen <intervigil@gmail.com>
2016-10-31 02:14:35 +0200

    ido: Add GraphicBuffer shim for camera.vendor.msm8916.so
    Change-Id: Idb1860a05b3669581951ac303bdefbb8ff0c8a99

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -4,10 +4,11 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
     android/sensor.cpp \
-    gui/SensorManager.cpp
+    gui/SensorManager.cpp \
+    ui/GraphicBuffer.cpp
 
 LOCAL_C_INCLUDES := gui
-LOCAL_SHARED_LIBRARIES := libgui libutils liblog libbinder libandroid
+LOCAL_SHARED_LIBRARIES := libgui libutils liblog libbinder libandroid libui
 LOCAL_MODULE := libshim_camera
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 
