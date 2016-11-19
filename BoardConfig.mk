cd53a9414fcf080624e36ed5f56474b83e47c511
Anik1199 <anik9280@gmail.com>
2016-11-19 18:08:38 +0600

    ido: Enable cpusets once again
    Change-Id: I02dda8a9470201254977842757d800ef82359ec0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,6 +39,9 @@ TARGET_USES_64_BIT_BINDER := true
 TARGET_BOOTLOADER_BOARD_NAME := MSM8916
 TARGET_NO_BOOTLOADER := true
 
+# Cpusets
+ENABLE_CPUSETS := true
+
 # Kernel
 BOARD_KERNEL_CMDLINE := console=115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
 BOARD_KERNEL_BASE := 0x80000000
