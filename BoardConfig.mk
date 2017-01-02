fec35788b99605e4a8c60c600dfb90133fc4537e
Anik1199 <anik9280@gmail.com>
2017-01-03 03:23:05 +0600

    ido: Ship prebuilt wcnss_service
    * Don't build libwcnss_qmi
    
    Change-Id: I0429629f4d873914534a23bce050b1ee8bbaa2f6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -175,14 +175,11 @@ TARGET_HAVE_SIGNED_VENUS_FW := true
 # Wifi
 BOARD_HAS_QCOM_WLAN              := true
 BOARD_HAS_QCOM_WLAN_SDK          := true
-BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_qcwcn
 BOARD_WLAN_DEVICE                := qcwcn
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
-TARGET_USES_WCNSS_CTRL           := true
-TARGET_USES_QCOM_WCNSS_QMI       := true
-TARGET_PROVIDES_WCNSS_QMI        := true
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 WIFI_DRIVER_FW_PATH_AP           := "ap"
 WIFI_DRIVER_FW_PATH_STA          := "sta"
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
