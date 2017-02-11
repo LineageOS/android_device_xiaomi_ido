9a1e8c2d241c33da06a2c91ad97ca76638818c21
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-02-11 20:35:17 +0800

    ido: camera: Fix GNU syntax to work with clang, fix other issues found by clang
    Change-Id: I07502f947b6c4a5188c3bb65d21d89276183882c

--- camera/QCamera2/HAL/QCameraParameters.cpp
+++ camera/QCamera2/HAL/QCameraParameters.cpp
@@ -3763,7 +3763,7 @@ int32_t QCameraParameters::setNumOfSnapshot()
                         ALOGE("%s: No memory for prop", __func__);
                         return NO_MEMORY;
                     }
-                    strlcpy(prop, str_val, strlen(str_val) + 1);
+                    strlcpy(prop, str_val, strlen(prop));
                     char *saveptr = NULL;
                     char *token = strtok_r(prop, ",", &saveptr);
                     while (token != NULL) {
@@ -6665,7 +6665,7 @@ int32_t QCameraParameters::parseGains(const char *gainStr, double &r_gain,
         ALOGE("%s: No memory for gains", __func__);
         return NO_MEMORY;
     }
-    strlcpy(gains, gainStr, strlen(gainStr) + 1);
+    strlcpy(gains, gainStr, strlen(gains));
     char *token = strtok_r(gains, ",", &saveptr);
     if (NULL == token) {
         ALOGE("%s:%d: strtok_r fails to find delimit", __func__,__LINE__);
@@ -8661,7 +8661,7 @@ uint8_t QCameraParameters::getBurstCountForAdvancedCapture()
               ALOGE("%s: No memory for prop", __func__);
               return NO_MEMORY;
           }
-          strlcpy(prop, str_val, strlen(str_val) + 1);
+          strlcpy(prop, str_val, strlen(prop));
           char *saveptr = NULL;
           char *token = strtok_r(prop, ",", &saveptr);
           while (token != NULL) {
@@ -10045,11 +10045,9 @@ int32_t QCameraParameters::commitParamChanges()
  *
  * RETURN     : none
  *==========================================================================*/
-QCameraReprocScaleParam::QCameraReprocScaleParam(QCameraParameters *parent)
-  : mParent(parent),
-    mScaleEnabled(false),
+QCameraReprocScaleParam::QCameraReprocScaleParam(QCameraParameters* /*parent*/)
+  : mScaleEnabled(false),
     mIsUnderScaling(false),
-    mScaleDirection(0),
     mNeedScaleCnt(0),
     mSensorSizeTblCnt(0),
     mSensorSizeTbl(NULL),
