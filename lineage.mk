af9a83fb8ad4ab6b7070900b88d2ea051738c158
Albert I <krascgq@outlook.co.id>
2017-05-06 13:49:28 +0000

    ido: Import build fingerprint from V8.1.3.0.LAIMIDI
    * Xiaomi/ido/ido:5.1.1/LMY47V/V8.1.3.0.LAIMIDI:user/release-keys
    * Not using latest firmware's fingerprint since it fails SafetyNet CTS check.
    
    Change-Id: I601c750651cc3845f70bb49a15dcffa8d22ef67e
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- lineage.mk
+++ lineage.mk
@@ -24,3 +24,8 @@ PRODUCT_NAME := lineage_ido
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Build fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="Xiaomi/ido/ido:5.1.1/LMY47V/V8.1.3.0.LAIMIDI:user/release-keys" \
+    PRIVATE_BUILD_DESC="ido-user 5.1.1 LMY47V V8.1.3.0.LAIMIDI release-keys"
