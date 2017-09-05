0754a6168ea1e921b51ec75784610f1bba1fed35
Anik1199 <anik@lineageos.org>
2017-09-05 23:12:20 +0600

    ido: Remove unused GraphicBuffer shim
    Change-Id: I7eed9b2b58e3e48eaa698730521aec7959fcee43

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -4,11 +4,10 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
     android/sensor.cpp \
-    gui/SensorManager.cpp \
-    ui/GraphicBuffer.cpp
+    gui/SensorManager.cpp
 
 LOCAL_C_INCLUDES := gui
-LOCAL_SHARED_LIBRARIES := libgui libutils liblog libbinder libandroid libui
+LOCAL_SHARED_LIBRARIES := libgui libutils liblog libbinder libandroid
 LOCAL_MODULE := libshim_camera
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 
