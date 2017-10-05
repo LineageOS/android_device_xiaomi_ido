feb3a8cd8871a3dbe22742fb0a5c1189da9c7b64
Anik1199 <anik@lineageos.org>
2017-10-05 16:43:00 +0600

    ido: Cleanup bluetooth header
    Change-Id: If49422a8240805ac68976f6f483399a4f9df2984

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -19,13 +19,11 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-#define BTM_DEF_LOCAL_NAME   "Xiaomi Redmi 3"
-// Disables read remote device feature
-#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
-#define MAX_ACL_CONNECTIONS    7
-#define MAX_L2CAP_CHANNELS    16
-#define BLE_VND_INCLUDED   TRUE
-// skips conn update at conn completion
-#define BTA_BLE_SKIP_CONN_UPD  FALSE
-#define BLE_PERIPHERAL_ADV_NAME  TRUE
+#define BTM_DEF_LOCAL_NAME        "Xiaomi Redmi 3"
+
+#define BLE_VND_INCLUDED          TRUE
+#define BLUETOOTH_QTI_SW          TRUE
+#define BT_CLEAN_TURN_ON_DISABLED TRUE
+#define MAX_ACL_CONNECTIONS       7
+
 #endif
