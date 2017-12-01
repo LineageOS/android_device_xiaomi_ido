ce610d248c774fea0e7e9d0b8f4549c5ed660cfa
Anik1199 <anik@lineageos.org>
2017-12-01 22:31:27 +0600

    ido: Update blob extraction scripts from vendor template
    Change-Id: Iea0a8472fd76388138996ff3166df478bc05eeea

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-#           (C) 2017 The LineageOS Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -21,7 +21,9 @@ set -e
 DEVICE=ido
 VENDOR=xiaomi
 
-# Load extractutils and do some sanity checks
+INITIAL_COPYRIGHT_YEAR=2016
+
+# Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
@@ -40,8 +42,7 @@ setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
 # Copyright headers and guards
 write_headers
 
-# The blobs
 write_makefiles "$MY_DIR"/proprietary-files.txt
 
-# We are done!
+# Finish
 write_footers
